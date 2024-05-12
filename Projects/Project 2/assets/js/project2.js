52d3a6ef7a0dc8d50940e1a282ab77de6722f722
EdinaJamakovic <edina.jamakovic@stu.ibu.edu>
2024-05-12 22:12:03 +0200

    Form validation, ajax

--- Projects/Project 2/assets/js/project2.js
+++ Projects/Project 2/assets/js/project2.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+    
+    //Form validation BEGINING
+    $.validator.addMethod("phoneBH", function(phone_number, element) {
+        console.log("Input:", phone_number);
+        phone_number = phone_number.replace(/[^\d]/g, "");;
+        console.log("Processed:", phone_number);
+        const regEx_phone = /^(?:387|0)\d{8,9}$/;
+        return phone_number.match(regEx_phone);
+      }, "Please enter a valid Bosnian phone number.");
+
     // Toggle cosmetic dentistry info
     $("#cosmetic-dentistry-info").click(function(){
-        $("#learn-more-1").toggle();
+        $("#facebookShow").toggle();
     });
 
     // Switch between light and dark theme
@@ -41,44 +51,98 @@ $(document).ready(function(){
             $("#success-message").show().delay(2000).fadeOut(3000);
         }
     });
-
     $("#appointment-form").validate({
         rules: {
             email: {
                 email: true,
             },
-
             phone: {
-                phone: true,
-            },
-
-            "name": {
+                // Define a custom method for validating phone numbers
+                phoneBH: true,
                 required: true,
             },
-
-            "services":{
+            name: {
+                required: true,
+            },
+            services: {
                 required: true,
             }
         },
-
         messages: {
             email: {
-                email: "Please enter a valid email adress.",
+                email: "Please enter a valid email address.",
+                required: "This field is required.",
             },
-
             phone: {
-                phone: "Please enter a valid phone number",
+                phoneBH: "Please enter a valid Bosnian phone number.",
+                required: "This field is required.",
             },
-            "name": {
-                required: "This field is required",
+            name: {
+                required: "This field is required.",
+            },
+            services: {
+                required: "This field is required.",
             },
 
-            "services": {
-                required: "This field is required",
-            },
         },
 
+        submitHandler: function(form){
+            form.reset()
+        },
 
+    });
+    //Form validation END
+
+    //accordion menu START
+    $("#accordion-toggle").click(function(){
+        let acc = $("#accordion-wrapper");
+        if (acc.css("display") == "block"){
+            acc.slideUp();
+        }
+        else {
+            acc.slideDown();
+        }
+    })
+
+    $(".accordion-item").click(function(event){
+        event.preventDefault();
+        let index = $(this).index();
+        let currentItem = $(".accordion-item").eq(index);
+        let currentBody = currentItem.find(".accordion-body");
+
+        if(currentBody.css("display") == "block") {
+            currentBody.slideUp();
+        }
+        else {
+            $(".accordion-body").slideUp();
+            currentBody.slideDown();
+        }
+        //accordion menu END 
+
+    })
+
+    $("#appointment-form").submit(function(event){
+        event.preventDefault();
+        let formData = {
+            fullName: $("#fullName").val(),
+            email: $("#email").val(),
+            phone: $("#phone").val(),
+            service: $("#service").val(),
+            message: $("#message").val()
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "../json/appointments.json",
+            data: formData,
+            dataType: "json",
+            success: function(data) {
+                console.log("Data added successfully: " + data);
+            },
+            error: function (responseTxt, statusTxt, xhr){
+                console.log("Error: " + xhr.responseText);
+            }
+        })
     })
 
 });
